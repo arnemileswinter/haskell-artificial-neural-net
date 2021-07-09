569735f5cc97cf44a755a2dc62accc30d44a118b
arne <contact@arnemileswinter.de>
2021-07-09 12:45:52 +0200

    removed unused code block from MonadRandom

--- src/Util/MonadRandom.hs
+++ src/Util/MonadRandom.hs
@@ -11,57 +11,46 @@ module Util.MonadRandom
 import System.Random (RandomGen, Random (random, randomR))
 import Control.Monad (replicateM)
 
-newtype MonadRandom g a = MonadRandom {runRandom' :: g -> (a,g)}
+-- | Useful to generate plenty of random values withohut carrying the generator along.
+newtype MonadRandom g a = MonadRandom {unRandom :: g -> (a,g)}
 
+-- | Leave the monadic context, yielding the random number generator and the generated output.
 runRandom :: g -> MonadRandom g a -> (a, g)
-runRandom = flip runRandom'
+runRandom = flip unRandom
 
 instance (RandomGen g) => Functor (MonadRandom g) where
-        aToB `fmap` m = MonadRandom $ \g -> let (a, g') = runRandom' m g
+        aToB `fmap` m = MonadRandom $ \g -> let (a, g') = unRandom m g
                                             in (aToB a, g')
 
 instance (RandomGen g) => Applicative (MonadRandom g) where
         pure a = MonadRandom $ \g -> (a,g)
         mAtoB <*> m = MonadRandom $ \g ->
-                let (aToB, g') = runRandom' mAtoB g
-                    (a, g'') = runRandom' m g'
+                let (aToB, g') = unRandom mAtoB g
+                    (a, g'') = unRandom m g'
                 in (aToB a,g'')
 
 instance (RandomGen g) => Monad (MonadRandom g) where
         mA >>= aToMb = MonadRandom $ \g ->
-                let (a,g') = runRandom' mA g
-                in runRandom' (aToMb a) g'
+                let (a,g') = unRandom mA g
+                in unRandom (aToMb a) g'
 
+-- | Retrieve a single random
 getRandom :: (RandomGen g, Random a) => MonadRandom g a
 getRandom = MonadRandom $ \g -> random g
 
-getRandomR :: (RandomGen g, Random a) => (a, a) -> MonadRandom g a
+-- | Generate a single random within a specific range
+getRandomR :: (RandomGen g, Random a) => 
+              (a, a) -- ^ The lower bound and upper bound of the randoms to generate.
+              -> MonadRandom g a
 getRandomR r = MonadRandom $ \g -> randomR r g
 
+-- | Generate a list of randoms.
 getRandoms :: (RandomGen g, Random a) => Int -> MonadRandom g [a]
 getRandoms n = replicateM n getRandom
 
-getRandomRs :: (RandomGen g, Random a) => (a, a) -> Int -> MonadRandom g [a]
+-- | Generate a list of randoms
+getRandomRs :: (RandomGen g, Random a) => 
+               (a, a) -- ^ The lower bound and upper bound of the randoms to generate.
+               -> Int -- ^ The amount of randoms to generate.
+               -> MonadRandom g [a]
 getRandomRs r n = replicateM n (getRandomR r)
-
-{-
-data MonadRandom g a = NewRandom {unMonadRandom::a}
-                       | WithGen {unMonadRandom::a, getGen::g}
-
-instance (RandomGen g) => Functor (MonadRandom g) where 
-        aToB `fmap` NewRandom a   = NewRandom $ aToB a
-        aToB `fmap` (WithGen a g) = WithGen (aToB a) g
-
-instance (RandomGen g) => Applicative (MonadRandom g) where
-        pure = NewRandom
-        NewRandom aToB <*> NewRandom a  = NewRandom $ aToB a
-        WithGen aToB g <*> WithGen a g' = WithGen (aToB a) g'
-        WithGen aToB g <*> NewRandom a  = WithGen (aToB a) g
-        NewRandom aToB <*> WithGen a g' = WithGen (aToB a) g'
-
-instance (RandomGen g) => Monad (MonadRandom g) where
-        (NewRandom a) >>= aToM = aToM a
-        (WithGen a g) >>= aToM = case aToM a of 
-                                      NewRandom b -> WithGen b g
-                                      WithGen b g' -> WithGen b g'
--}
