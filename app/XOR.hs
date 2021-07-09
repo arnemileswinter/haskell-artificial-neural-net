38625fface86c9cd5892bd921feeae799be7dcee
arne <contact@arnemileswinter.de>
2021-07-09 13:03:10 +0200

    some more comments in example

--- app/XOR.hs
+++ app/XOR.hs
@@ -7,12 +7,20 @@ import ML.NeuralNet (newNeuralNet, ActivationFunction(..),predict,train')
 main :: IO ()
 main = do
      g <- newStdGen
-     let (n, _') = runRandom g $ newNeuralNet (0.3) 1 2 [(ReLU, 3)] (Sigmoid, 1)
-     let nt = train' n $ take 1000000 $ cycle $ [([[1],[1]],[0])
-                       ,([[1],[0]],[1])
-                       ,([[0],[1]],[1])
-                       ,([[0],[0]],[0])
-                       ]
+     -- get a NeuralNet
+     let (n, _) = runRandom g $ newNeuralNet (0.3)         -- Learning rate is 0.3
+                                              1             -- There is one incoming edge for each input neuron. 
+                                              2             -- There are 2 input neurons
+                                              [(ReLU, 5)]   -- There is a single hidden layer with the ReLU activation and 5 neurons
+                                              (Logistic, 1) -- The output layer is a single sigmoid-activated neuron
+
+     -- train it with a million samples that describe the XOR function.
+     let nt = train' n $ take (10^(6::Int)) $ cycle [([[1],[1]],[0])
+                                              ,([[1],[0]],[1])
+                                              ,([[0],[1]],[1])
+                                              ,([[0],[0]],[0])
+                                              ]
+     -- print the outcomes.
      print $ predict nt [[1],[1]]
      print $ predict nt [[0],[1]]
      print $ predict nt [[1],[0]]
